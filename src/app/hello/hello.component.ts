d98c09abe8a12c4f690572c05858e32b54da90f4
delficasarino <delficasarino@gmail.com>
2023-06-30 11:47:03 -0300

    2nd

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,11 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-    selector: 'custom-hello',
-    template: `<p>
-                Hello world!
-              </p>`
+  selector: "custom-hello",
+  templateUrl:
+    "./hello.component.html" /* cuando el componente tiene informacion que esta en el html */,
 })
-export class HelloComponent{
-
-}
+export class HelloComponent {}
