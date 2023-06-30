5807f78f4406a82afa56ff8ca3fda4aa662cdf91
delficasarino <delficasarino@gmail.com>
2023-06-30 13:34:35 -0300

    interpolacion

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-    selector: 'custom-hello',
-    templateUrl: './hello.component.html',
-    styleUrls: ['./hello.component.css']
+  selector: "custom-hello",
+  templateUrl: "./hello.component.html",
+  styleUrls: ["./hello.component.css"],
 })
-export class HelloComponent{
-    public name:string;
+export class HelloComponent {
+  public name: string;
 
-    constructor(){
-        this.name = 'Enrique';
-    }
+  constructor() {
+    this.name = "Delfi";
+  }
 }
