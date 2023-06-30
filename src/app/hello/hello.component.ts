13a14e23aeeefb757903aeec7333b9386802e9e4
delficasarino <delficasarino@gmail.com>
2023-06-30 12:11:01 -0300

    3rd

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-    selector: 'custom-hello',
-    templateUrl: './hello.component.html'
+  selector: "custom-hello",
+  templateUrl: "./hello.component.html",
+  styleUrls: ["./hello.component.css"],
 })
-export class HelloComponent{
-
-}
+export class HelloComponent {}
