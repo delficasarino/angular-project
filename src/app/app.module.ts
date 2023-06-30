250b14d70d90e4a6865de439dce789c9cc70a718
delficasarino <delficasarino@gmail.com>
2023-06-30 11:31:32 -0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+
+import { HelloComponent } from "./hello/hello.component";
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+    AppComponent,
+    HelloComponent,
+  ] /* llamo a los componentes que quiero render */,
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "custom-hello",
+  template: `<p>Hello World</p>`,
+}) /* selector es el nombre que voy a llamar luego, template es lo que va a aparecer */
+export class HelloComponent {}
