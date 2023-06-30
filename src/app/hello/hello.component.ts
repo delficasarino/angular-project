e08695e83f6a90a8ae3e52222583acded0089340
delficasarino <delficasarino@gmail.com>
2023-06-30 14:36:00 -0300

    ngModel

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,30 +1,25 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-    selector: 'custom-hello',
-    templateUrl: './hello.component.html',
-    styleUrls: ['./hello.component.css']
+  selector: "custom-hello",
+  templateUrl: "./hello.component.html",
+  styleUrls: ["./hello.component.css"],
 })
-export class HelloComponent{
-    @Input() name:string;
-    @Output() sayHello:EventEmitter<string> = new EventEmitter<string>();
+export class HelloComponent {
+  @Input() name: string;
+  @Output() sayHello: EventEmitter<string> = new EventEmitter<string>();
 
-    public namesList:Array<string> = [];
-    public inputName:string = 'Jhon';
+  public namesList: Array<string> = [];
+  public inputName: string = "";
 
-    constructor(){ }
+  constructor() {}
 
-    onClick(){
-        this.sayHello.emit('Hello!');
-    }
+  onClick() {
+    this.sayHello.emit("Hello!");
+  }
 
-    addName(){
-        this.namesList.push(this.inputName);
-        console.log(this.namesList);
-    }
-
-    updateInputName(event:any){
-        this.inputName = event.target.value;
-        console.log(this.inputName);
-    }
+  addName() {
+    this.namesList.push(this.inputName);
+    console.log(this.namesList);
+  }
 }
