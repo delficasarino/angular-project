9f6391c3c13f415e317c0e484bd454340fc65a05
delficasarino <delficasarino@gmail.com>
2023-06-30 14:23:36 -0300

    input biderec

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,17 +1,31 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-    selector: 'custom-hello',
-    templateUrl: './hello.component.html',
-    styleUrls: ['./hello.component.css']
+  selector: "custom-hello",
+  templateUrl: "./hello.component.html",
+  styleUrls: ["./hello.component.css"],
 })
-export class HelloComponent{
-    @Input() name:string;
-    @Output() sayHello:EventEmitter<string> = new EventEmitter<string>();
+export class HelloComponent {
+  @Input() name: string;
+  @Output() sayHello: EventEmitter<string> = new EventEmitter<string>();
 
-    constructor(){ }
+  public namesList: Array<string> = [];
 
-    onClick(){
-        this.sayHello.emit('Hello!');
-    }
+  public inputName: string = "Pedro";
+  constructor() {}
+
+  onClick() {
+    this.sayHello.emit("Hello!");
+  }
+
+  addName() {
+    this.namesList.push(this.inputName);
+    this.inputName = "";
+    console.log(this.namesList);
+  }
+
+  updateInputName(event: any) {
+    this.inputName = event.target.value;
+    console.log(this.inputName);
+  }
 }
